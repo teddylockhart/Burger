e1ceaf22ca0dd06310c8fadb03b6651b74ef3a8e
teddylockhart <teddy.lockhart@gmail.com>
2017-09-27 21:15:00 -0700

    Repo Back-Up Upload

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('In-N-Out Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hodads Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Five Guys Burger');
+
